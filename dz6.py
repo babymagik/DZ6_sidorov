fa0644f494db6b10c2b1a6c285aac3361ad2a539
babymagik <91948180+babymagik@users.noreply.github.com>
2021-11-09 11:25:58 +0300

    Update dz6.py

--- dz6.py
+++ dz6.py
@@ -2,7 +2,7 @@ with open ('nginx_logs.txt') as f:
     data = []
     for line in f:
         splitted = line.split()
-        data.append((splitted[0], splitted[5].replace('"', ''), splitted[6])
+        data.append((splitted[0], splitted[5].replace('"', ''), splitted[6]))
 print(data)
 
 
